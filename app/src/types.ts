0228d40cd7d8453a4a034ec8be74d4423341502f
s.kormilin <ubscoder@gmail.com>
2022-09-13 13:29:44 +0300

    feature: add diet type

--- app/src/types.ts
+++ app/src/types.ts
@@ -1,15 +1,18 @@
-type Status = 'Want to Try' | 'Recommended' | 'Do Not Recommend'
-
 export type Restaurant = {
   id: string
   name: string
   address: string
   website: string
   status: Status
+  diet?: Diet
 }
 
 export type Dish = {
   id: string
   name: string
   status: Status
+  diet?: Diet
 }
+
+type Status = 'Want to Try' | 'Recommended' | 'Do Not Recommend'
+type Diet = 'Vegetarian' | 'Pescetarian' | 'Keto' | ''
